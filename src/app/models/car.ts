19d435744636694cab9a386f74c63a734e5940e3
sema karbav <s-karbav@hotmail.com>
2021-04-30 04:29:43 +0300

    ProjectReCar-FrontEnd

---
+++ src/app/models/car.ts
@@ -0,0 +1,12 @@
+export interface Car{
+    id:number;
+    colorId:number;
+    brandId:number;
+    brandName:string;
+    colorName:string;
+    carName:string;
+    modelYear:string;
+    dailyPrice:number;
+    description:string;
+   
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,21 @@
+export interface Rental{
+    id:number;
+    carId:number;
+    customerId:number;
+    userId:number;
+    rentDate:string;
+    returnDate:string;
+    companyName:string;
+    firstName:string;
+    lastName:string;
+    email:string;
+    carName:string
+    brandId:number;
+    brandName:string;
+    colorId:number
+    colorName:string;
+    modelYear: string;     
+    dailyPrice: number;
+    
+
+}
